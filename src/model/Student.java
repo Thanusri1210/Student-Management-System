1b0d19a36a964022e9830c51438d20e0bff71098
Thanusri1210 <uppanvarthanusri007@gmail.com>
2026-03-01 21:50:34 +0530

    UpdateStudent.java

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,5 +1,21 @@
 package model;
 
 public class Student {
-    
+    int age;
+    int id;
+    int marks;
+    String name;
+    String departmet;
+    String grade;
+    public Student(){
+
+    }
+    public Student(int age,int id,int marks,String name,String department,String grade){
+        this.age=age;
+        this.id=id;
+        this.marks=marks;
+        this.name=name;
+        this.departmet=department;
+        this.grade=grade;
+    }
 }
