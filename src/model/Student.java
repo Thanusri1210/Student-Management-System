0a087fc63efaec0b55c8f499d123c1abbacf24d4
Thanusri1210 <uppanvarthanusri007@gmail.com>
2026-03-02 20:07:34 +0530

    student

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,21 +1,45 @@
 package model;
 
 public class Student {
-    int age;
-    int id;
-    int marks;
-    String name;
-    String department;
-    String grade;
+    private int age;
+    private int id;
+    private int marks;
+    private String name;
+    private String department;
+    private char grade;
     public Student(){
 
     }
-    public Student(int age,int id,int marks,String name,String department,String grade){
+    public Student(int age,int id,int marks,String name,String department)
+    {
         this.age=age;
         this.id=id;
         this.marks=marks;
         this.name=name;
         this.department=department;
-        this.grade=grade;
+        if(marks>=90)
+        {
+           this.grade='A';
+        }
+        else if(marks>=80)
+        {
+           this.grade='B';
+        }
+        else if(marks>=70)
+        {
+            this.grade='C';
+        }
+        else if(marks>=60)
+        {
+           this.grade='D';
+        }
+        else if(marks>=50)
+        {
+            this.grade='E';
+        }
+        else
+        {
+            this.grade='F';
+        }
     }
 }
